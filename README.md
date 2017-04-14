45d9c3376e8611f6f5917adb41ece17b3ecb7541
444xxk <penztest@tuta.io>
2017-04-14 15:34:35 +0200

    more improve

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # shotovuln
 
-An offensive bash script which tries to find automatically GENERIC privesc vulnerabilities and issues on *Nix systems. 
+An offensive bash script which tries to find automatically GENERIC privesc vulnerabilities and issues on Linux systems. 
+The tool will try to focus only on useful info.
 
-# usage 
+
+# usage
 
 `./shotovuln.sh`
-
--- shotovuln.sh
+++ shotovuln.sh
@@ -27,12 +27,12 @@ echo "Brute forcing local users via su, download / compile sucrack"
 # wget
 #./sucrack wordlist.txt
 
-echo "Scanning localhost ports for SSH like and other protocols"
+echo "Scanning localhost ports for SSH detection and brute force";
 nc -z -v 127.0.0.1 1-65535;
-ssh
+echo "Select the SSH port";
 
-echo "Do we have access to dmesg?"
-dmesg;
+#echo "Do we have access to dmesg and check privesc related information ?"
+#dmesg;
 
 
 
@@ -43,10 +43,14 @@ echo "### 2. Auditing file and folders permissions to privesc"
 echo "Root owned files in non root owned directory, ie. non root user can replace root owned files"
 for x in $(find /var -type f -user root 2>/dev/null -exec dirname {} + | sort -u); do (echo -n "$x is owned by " && stat -c %U "$x") | grep -v 'root'; done
 
-echo "Checking tmp files for secrets";
+echo "Writable directory in default PATH, ie. ..."
+path=$(echo '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' | tr ':' ' ');
+
+
+echo "Checking tmp files for passwords or secrets";
 find /tmp/ -type f -size +0 -exec grep -i --color 'secret/|password/|' {} + 2>/dev/null;
 
-echo "Writable folders for everyone, usefull for next steps"
+echo "Saving writable folders for everyone, useful for next steps"
 writable=$(find / -type d -perm /o+w 2>/dev/null);
 
 
@@ -64,36 +68,51 @@ echo "SUID folders writable by others, ie. other can get user rights by writing 
 find / -type d -perm /u+s -perm /o+w -exec ls -alhd {} + 2>/dev/null;
 
 echo "Test SUID conf files for error based info disclosure"
-echo "TO DO";
+echo "[TO DO]";
 
 echo "Generating SUID logs ... you might receive some pop ups and error message since we are starting all SUID binaries.";
 mkdir -p ~/.shotologs;
 for x in $(find / -perm /4000 2>/dev/null); do timeout 30s strace "$x" 2>~/.shotologs/$(basename "$x").stracelog; done;
 
-echo "Relative path opening in suid binaries, ie. you can fool the suid binary to open arbitrary file"
-grep 'open("\.' ~/.shotologs/* --color;
-grep 'open(' ~/.shotologs/* | grep -v 'open("/';
+echo "Relative path opening in suid binaries, ie. you can fool the suid binary to open arbitrary file."
+grep -n 'open("\.' ~/.shotologs/* --color;
+grep -n 'open(' ~/.shotologs/* | grep -v 'open("/';
 
-echo "Environment variables used in suid binaries, ie. untrusted use of env variables"
-grep --color "getenv(" logs/*;
+echo "Environment variables used in suid binaries, ie. untrusted use of env variables."
+grep -n --color "getenv(" logs/*;
 
-echo "Exec used in suid binaries, ie. untrusted use of PATH potentialy"
-grep --color execve logs/*;
+echo "Exec used in suid binaries, ie. untrusted use of PATH potentialy."
+grep -n --color execve logs/*;
 
 
 
 echo "";
 
 echo -e "\n"
-echo "### 4. Specific edge cases which enable you to change privilege"
+echo "### 4. Specific edge cases which enable you to change privilege."
 
 echo "Apache symlink test"
-find / -name apache*.conf -exec grep -i symlink --color {} +; 2>/dev/null
+find / -name apache*.conf -exec echo {} + -exec grep -i symlink --color {} + 2>/dev/null;
 
 echo "Pythonpath or environment var issues"
 python -c "import sys; print '\n'.join(sys.path);"
 
+# we might need to create a matrix of user privs
+# user1 > user2 > user9 > group1 > rootgroup > root
+
+
 
 echo ""
 echo "### 5. Init.d script auditing"
-find /etc/init.d/ -exec grep --color /tmp {} + ;
+
+echo "Usage of predictable or fixed files in a writable folder"
+echo $writable;
+grep -n -R --color ' /tmp' /etc/init.d/*;
+
+
+
+
+echo "### 6. Conf files password disclosure and password reuse"
+grep -v '^$\|^\s*\#' /etc/*.conf  | grep -i --color "password" -A1;
+grep -v '^$\|^\s*\#' /etc/*/*.conf  | grep -i --color "password" -A1;
+echo "storing passwords found ..."
