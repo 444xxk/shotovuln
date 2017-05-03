b3e669f085fbe00fa6520b222f9f921aa79737b6
xxk444 <undicslosed@undiclsosed.undiclossed>
2017-05-03 07:08:29 -0400

    v0.2 bugfixes

--- shotovuln.sh
+++ shotovuln.sh
@@ -36,23 +36,26 @@ echo "[o] Saving writable folders for everyone, useful for next steps";
 writabledirs=$(find / -type d -perm /o+w ! -path "*mqueue*" 2>/dev/null);
 writedir=$(echo "$writabledirs" | head -n1);
 echo "[o] Will use as writable dir: $writedir";
+
 currentuser=$(id);
 echo "[o] Current user and privileges is: $currentuser";
 echo "[o] Getting quality short password wordlist from internet...";
 
-
-if [ ! -f "$writedir/.wordlist" ]; then
-wget -qO "$writedir/.wordlist" "https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top220-probable.txt"; # 200 to be fast
-passwords="$writedir/.wordlist";
+if [ $dwl == true ]; then
+ if [ ! -f "$writedir/.wordlist" ]; then
+ wget -qO "$writedir/.wordlist" "https://raw.githubusercontent.com/berzerk0/Probable-Wordlists/master/Real-Passwords/Top220-probable.txt"; # 200 to be fast
+ fi;
 fi;
+
 
 echo "[o] Getting valid users for login";
 validusers=$(grep -v '/false' /etc/passwd | grep -v '/nologin' | cut -d ':' -f1);
 
-# python and pip needed on the box TODO test here
-if ( which python );  then pip install pexpect; pythonandpip=true; fi
-
-
+if [ $brute == true ]; then
+ passwords="$writedir/.wordlist";
+ # python and pip needed on the box TODO test here
+ if ( which python );  then pip install pexpect; pythonandpip=true; fi;
+fi;
 
 
 echo "";
@@ -116,9 +119,9 @@ pathstotest=$(echo "$PATH" | tr ':' '\n');
 for path in $pathstotest; do find "$path" 2>/dev/null -type d -perm /o+w -exec ls -alhd {} +; done
 
 echo "[x] Checking usual temporary folder for passwords or secrets and storing them, i.e. other can use the password to elevate or test password reuse"; # example CVE-xxx CWE info disclosure
-find /tmp/ -type f -size +0 -exec grep -i --color 'secret/|password/|' {} + 2>/dev/null;
-# TODO this step outputs useless information sometimes so filter it better
-# need to be reviewed and confirmed useful
+find /tmp/  2>/dev/null -type f -size +0 -exec grep -i 'secret/|password/|' {} +;
+find /dev/shm  2>/dev/null -type f -size +0 -exec grep -i 'secret/|password/|' {} +;
+# TODO hash find in this content
 
 echo "[x] Checking crontab script protection, ie. other can write to crontab scripts" #example CVE-xxx
 for user in $validusers; do
@@ -133,19 +136,15 @@ done
 
 echo "";
 echo "### 3. Auditing SUID and SUID operations without arguments provided"
-# TODO be careful not to kill network
+# TODO be careful not to kill network with SUID 
 ### https://www.pentestpartners.com/blog/exploiting-suid-executables/;
-
 echo "[x] SGID folders writable by others, ie. other can get group rights by writing to it"
 find / -type d -perm /g+s -perm /o+w -exec ls -alhd {} + 2>/dev/null;
-
 echo "SUID folders writable by others, ie. other can get user rights by writing to it"
 find / -type d -perm /u+s -perm /o+w -exec ls -alhd {} + 2>/dev/null;
-
 # TODO echo "Test SUID conf files for error based info disclosure"
 # TODO code it --conf / -c / grep conf in help
 # example ./suidbinary -conf /etc/shadow outputs the user hashes
-
 echo "Generating SUID logs ... you might receive some pop ups and error message since we are starting all SUID binaries.";
 mkdir -p "$writedir/.shotologs";
 
@@ -158,18 +157,13 @@ basename=$(basename "$suid");
 timeout 13s strace "$suid" 2>"$writedir"/.shotologs/"$basename".stracelog 1>/dev/null ;
 done < "$writedir/.suidbinaries";
 
-
-
-
 # add this generic vuln , SUID root loading from writable dir :/
 # https://www.exploit-db.com/exploits/41907/
 echo "[x] Relative path opening in SUID binaries, ie. other can fool the SUID binary to open arbitrary file." #example CVE-xxx
 grep -n 'open("\.' "$writedir"/.shotologs/* --color;
 grep -n 'open(' "$writedir"/.shotologs/* | grep -v 'open("/';
-
 echo "[x] Environment variables used in suid binaries, ie. other can inject into env, untrusted use of env variables." #example CVE-xxx
 grep -n --color "getenv(" "$writedir"/.shotologs/*;
-
 echo "[x] Exec used in SUID binaries, ie. other can fool SUID use of PATH, untrusted use of PATH." #example CVE-xxx
 grep -n --color 'execve(\.' "$writedir"/.shotologs/*;
 
@@ -180,10 +174,8 @@ grep -n --color 'execve(\.' "$writedir"/.shotologs/*;
 
 echo "";
 echo "### 4. Specific edge cases which enable you to change privilege"
-
 echo "[x] Apache symlink test, ie. allows other to check files and folders of other users using the shared apache account"; #example no CVE but feature
 find / -name "apache*.conf" -exec echo {} + -exec grep -i symlink --color {} + 2>/dev/null;
-
 echo "[x] Pythonpath variable issues, ie. if PATH is vulnerable and a python privilege script runs, other can inject into its PATH"; # example CVE-xxx
 pythonpath=$(python -c "import sys; print '\n'.join(sys.path);")
 for path in $pythonpath; do find "$path" 2>/dev/null -type d -perm /o+w -exec ls -alhd {} +; done
@@ -216,7 +208,7 @@ usedbyinit="$(grep -n -R --color ' /tmp' /etc/init.d/* | sort -u)";
 echo "";
 echo -e "### 6. Conf files password disclosure and password reuse";
 echo "[x] Checking readable passwords used in .conf files, ie. other can read and use then or try password reuse";
-find / -name "*.conf" 2>/dev/null -exec grep -n -i "password =\|password=\|password :\|password:" {} +;
+find / 2>/dev/null -name "*.conf"  -exec grep -n -i "password =\|password=\|password :\|password:" {} +;
 # TODO filter false positives, filter comments
 
 
@@ -224,12 +216,16 @@ find / -name "*.conf" 2>/dev/null -exec grep -n -i "password =\|password=\|passw
 
 
 echo "";
-echo -e "$ORANGE### 7. Log file information disclosure $NOCOLOR";
-
+echo "### 7. Log file information disclosure";
 echo "[x] Checking history files and harvesting info, ie. other can read password and try password reuse"; # example CWE file folder permission
-find / -name "*history" 2>/dev/null -exec grep -n -i "--password\|--pass\|-pass\|-p" {} +;
+find / 2>/dev/null -name "*history"  -exec grep -n -i "--password\|--pass\|-pass\|-p" {} +;
 # $validusers history grepping
 
+
+echo "";
+echo "### 8. Database file information disclosure";
+echo "[x] Checking passwords inside local databases file"
+find / 2>/dev/null -name "*.sqlite" -readable -exec grep -i 'pass' {} +;
 
 
 
