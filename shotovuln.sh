79d730cba21f4d7dbcce1c178a17abd10363c635
xxk444 <undicslosed@undiclsosed.undiclossed>
2017-05-03 05:32:01 -0400

    adding examples and some fixes

--- shotovuln.sh
+++ shotovuln.sh
@@ -12,13 +12,13 @@ echo "SHOTOVULN v0.1"
 echo -e "$GREEN Senseiiii *0* show me the path to $RED R00T $BLUE *o* ! $NOCOLOR";
 echo "";
 echo "Please run this script as low privilege user :]";
-echo -e "$ORANGE Usage: $NOCOLOR $0 $GREEN [currentuserpassword] [nonbrute] [nodwl]";
+echo -e "$ORANGE Usage: $NOCOLOR $0 $GREEN [currentuserpassword] [nobrute] [nodwl]";
 echo "";
 
 # PHILOSOPHY / IDEAS
 # - non interactive
 # - stealth , try not to touch drive except if needed, try to run everything in memory as much as possible
-# - do not output useless information
+# - do not output useless information, only valid vuln
 # - run as low privilege user
 # - show clear path to root
 # requirement on the compromised box : *nix OS, bash , python , pip
@@ -42,14 +42,12 @@ fi;
 echo " Getting valid users for login";
 validusers=$(grep -v '/false' /etc/passwd | grep -v '/nologin' | cut -d ':' -f1);
 # debug
-echo "[debug] Valid users are:"
-echo "$validusers";
+# echo "[debug] Valid users are:"
+# echo "$validusers";
+
 
 # python and pip needed on the box TODO test here
-# pip install pexpect;
-# $pythonandpip=true | false
-
-
+if ( which python );  then pip install pexpect; pythonandpip=true; fi
 
 
 
@@ -80,11 +78,11 @@ echo "[debug]: $sshusers";
 
 echo "Scanning localhost ports for SSH detection and brute force";
 # TODO better ssh detection
-nc -z -v 127.0.0.1 22;
-nc -z -v 127.0.0.1 222;
-nc -z -v 127.0.0.1 2222;
-nc -z -v 127.0.0.1 22222;
-nc -z -v 127.0.0.1 10022;
+#nc -z -v 127.0.0.1 22;
+#nc -z -v 127.0.0.1 222;
+#nc -z -v 127.0.0.1 2222;
+#nc -z -v 127.0.0.1 22222;
+#nc -z -v 127.0.0.1 10022;
 
 echo "Now bruting valid users on SSH ports using ssh passcript";
 # ./tools/sshpassscript.sh "$passwords";
@@ -101,18 +99,28 @@ echo "Now bruting valid users on SSH ports using ssh passcript";
 echo "";
 echo -e "$ORANGE### 2. Auditing file and folders permissions to privesc $NOCOLOR"
 
-echo "Root owned files in non root owned directory, ie. other can replace root owned files"
+echo "Simply cating /etc/shadow and /etc/shadow derivatives, might be lucky";
+cat /etc/shadow 2>/dev/null;
+cat /etc/shadow.old 2>/dev/null;
+cat /etc/shadow.bak 2>/dev/null;
+
+echo "Checking private RSA keys in home folders"
+for user in $validusers; do cat "/home/$user/.ssh/id_rsa" 2>/dev/null; done
+# extended
+
+echo "Root owned files in non root owned directory, ie. other can replace root owned files" # example nginx vuln on debian
 for x in $(find /var -type f -user root 2>/dev/null -exec dirname {} + | sort -u); do (echo -n "$x is owned by " && stat -c %U "$x") | grep -v 'root'; done
 
-echo "Writable directory in default PATH, ie. users can tamper PATH of SUID for instance"
+echo "Writable directory in default PATH, ie. other can tamper PATH of automatically run scripts"
 pathtotest=$(echo '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' | tr ':' ' ');
 #for x in pathtotest; do ; done
 
 echo "Checking tmp files for passwords or secrets and storing them, i.e. other can use the password to elevate or test password reuse";
 find /tmp/ -type f -size +0 -exec grep -i --color 'secret/|password/|' {} + 2>/dev/null;
 # TODO this step outputs useless information sometimes so filter it better
+# need to be reviewed and confirmed useful
 
-
+# crontab check
 
 
 
@@ -129,7 +137,7 @@ find / -type d -perm /g+s -perm /o+w -exec ls -alhd {} + 2>/dev/null;
 echo "SUID folders writable by others, ie. other can get user rights by writing to it"
 find / -type d -perm /u+s -perm /o+w -exec ls -alhd {} + 2>/dev/null;
 
-echo "Test SUID conf files for error based info disclosure"
+# TODO echo "Test SUID conf files for error based info disclosure"
 # TODO code it --conf / -c / grep conf in help
 # example ./suidbinary -conf /etc/shadow outputs the user hashes
 
@@ -150,15 +158,16 @@ done < "$writedir/.suidbinaries";
 
 # add this generic vuln , SUID root loading from writable dir :/
 # https://www.exploit-db.com/exploits/41907/
-echo "Relative path opening in suid binaries, ie. you can fool the suid binary to open arbitrary file."
+echo "Relative path opening in SUID binaries, ie. other can fool the SUID binary to open arbitrary file." # example CVE-xxx
 grep -n 'open("\.' "$writedir"/.shotologs/* --color;
 grep -n 'open(' "$writedir"/.shotologs/* | grep -v 'open("/';
 
-echo "Environment variables used in suid binaries, ie. untrusted use of env variables."
+echo "Environment variables used in suid binaries, ie. other can inject into env, untrusted use of env variables." # example CVE-xxx
 grep -n --color "getenv(" "$writedir"/.shotologs/*;
 
-echo "Exec used in suid binaries, ie. untrusted use of PATH potentialy."
+echo "Exec used in SUID binaries, ie. other can fool SUID use of PATH, untrusted use of PATH." # example CVE-xxx
 grep -n --color 'execve(\.' "$writedir"/.shotologs/*;
+
 
 
 
@@ -167,11 +176,11 @@ grep -n --color 'execve(\.' "$writedir"/.shotologs/*;
 echo "";
 echo -e "$ORANGE### 4. Specific edge cases which enable you to change privilege. $NOCOLOR"
 
-echo "Apache symlink test, i.e. this allows you to check other users files and folders using the shared apache user";
-find / -name apache*.conf -exec echo {} + -exec grep -i symlink --color {} + 2>/dev/null;
+echo "Apache symlink test, i.e. allows other to check files and folders of other users using the shared apache account"; # example no CVE but feature
+find / -name "apache*.conf" -exec echo {} + -exec grep -i symlink --color {} + 2>/dev/null;
 
-echo "Pythonpath or environment var issues, i.e."
-python -c "import sys; print '\n'.join(sys.path);"
+echo "Pythonpath variable issues, i.e. if PATH is vulnerable and a python privilege script runs, other can inject into its PATH"; # example CVE-xxx
+pythonpath=$(python -c "import sys; print '\n'.join(sys.path);")
 
 
 
@@ -182,7 +191,7 @@ python -c "import sys; print '\n'.join(sys.path);"
 echo "";
 echo -e "$ORANGE### 5. Init.d script auditing $NOCOLOR";
 ### The problem is service (init.d) strips all environment variables but TERM, PATH and LANG which is a good thing
-echo "RC scripts pointing to a user controled directory, i.e. user can write to it and get privilege";
+echo "RC scripts pointing to vulnerable directory, i.e. other can write to it and get root privilege"; # example CVE-xxx 
 #grep -n --color '/' /etc/rc.local;
 echo "Init.d scripts using unfiltered environment variablesm, i.e. user can inject into it and get privilege";
 grep -n -R -v 'PATH=\|LANG=\|TERM=' /etc/init.d/* | grep --color "PATH\|LANG\|TERM";
@@ -202,7 +211,7 @@ usedbyinit="$(grep -n -R --color ' /tmp' /etc/init.d/* | sort -u)";
 echo "";
 echo -e "$ORANGE### 6. Conf files password disclosure and password reuse $NOCOLOR";
 echo "Checking passwords used in .conf files";
-find / -name "*.conf" 2>/dev/null -exec grep -i --color "password =\|password=\|password :\|password:" {} +;
+find / -name "*.conf" 2>/dev/null -exec grep -n -i --color "password =\|password=\|password :\|password:" {} +;
 # TODO filter false positives, filter comments
 
 
@@ -212,8 +221,8 @@ find / -name "*.conf" 2>/dev/null -exec grep -i --color "password =\|password=\|
 echo "";
 echo -e "$ORANGE### 7. Log file information disclosure $NOCOLOR";
 
-echo "Valid users history files harvesting info";
-historyfiles={1,2};
+echo "Checking history files and harvesting info";
+find / -name "*history" 2>/dev/null -exec grep -n -i --color "--password\|--pass\|-pass\|-p" {} +;
 # $validusers history grepping
 
 
